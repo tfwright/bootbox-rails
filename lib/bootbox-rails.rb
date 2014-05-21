6ca35508699159dcc1af0cfc7a1ced9575460122
Thomas Floyd Wright <tfwright@gmail.com>
2014-05-21 16:57:15 +0000

    Improves version check

--- lib/bootbox-rails.rb
+++ lib/bootbox-rails.rb
@@ -1,4 +1,4 @@
-if defined? Rails && Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR >= 1
+if defined? Rails && Gem::Version.new(Rails.version) >= Gem::Version.new("3.1.0")
   require "bootbox-rails/engine"
 else
   puts "You should use Rails 3.1 and higher with bootbox-rails!"
